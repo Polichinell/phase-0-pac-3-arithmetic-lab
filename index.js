4e772f240ac5c4570e1d67348000a4b2cb85c05b
Liselle <tsuqinoa@gmail.com>
2023-07-21 18:59:23 +0200

    done

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b
+  }
+
+function subtract(a, b) {
+    return a - b
+  }
+
+function multiply(a, b) {
+    return a * b
+  }
+
+function divide(a, b) {
+    return a / b
+  }
+
+function increment(n) {
+    let number = n
+    return number+=1
+  }
+
+function decrement(n) {
+    let number = n
+    return number-=1
+  }
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
